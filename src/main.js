bc607c2be156365b47942bed03b410e12c6d7139
Jordy Shryock <jordy1611@gmail.com>
2020-05-14 13:34:18 -0600

    Added functionility to make new cover button

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ var coverTitle = document.querySelector('.cover-title');
 var coverTagLine1 = document.querySelector('.tagline-1');
 var coverTagLine2 = document.querySelector('.tagline-2');
 var showRandomButton = document.querySelector('.random-cover-button');
-
+var makeNewButton = document.querySelector('.make-new-button');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var homeButton = document.querySelector('.home-button');
+var homeView = document.querySelector('.home-view');
+var formView = document.querySelector('.form-view');
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -17,6 +21,7 @@ var currentCover;
 // Add your event listeners here 👇
 window.addEventListener('load', randomizeCover);
 showRandomButton.addEventListener('click', randomizeCover);
+makeNewButton.addEventListener('click', showFormPage);
 
 // Create your event handlers and other functions here 👇
 
@@ -37,3 +42,12 @@ function randomizeCover() {
   coverTagLine2.innerText = randomTagline2;
   currentCover = new Cover(randomCover, randomTitle, randomTagline1, randomTagline2);
 };
+
+function showFormPage(event) {
+  event.preventDefault();
+  showRandomButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  homeButton.classList.remove('hidden');
+  homeView.classList.add('hidden');
+  formView.classList.remove('hidden');
+}
